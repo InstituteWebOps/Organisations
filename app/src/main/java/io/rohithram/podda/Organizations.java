2dc9bb7c59af84a99b84d1b9ebd6ca72cc0ec235
rohithram <ram234historyalex@gmail.com>
2017-07-02 11:59:25 +0530

    Made organisations activity and ui,but not fully

---
+++ app/src/main/java/io/rohithram/podda/Organizations.java
@@ -0,0 +1,13 @@
+package io.rohithram.podda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Organizations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.organisation);
+    }
+}
